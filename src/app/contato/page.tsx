e08bad8efbe485d65c975244bf3d6551a956b493
WeslleySORDev <weslleysordev@gmail.com>
2024-02-28 18:29:37 -0300

    feat: 0201 App Router

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contato',
+  description: 'Essa é a página contato',
+};
+export default function ContatoPage() {
+  return (
+    <main>
+      <h2>Contato</h2>
+    </main>
+  );
+}
---
+++ src/components/menu.tsx
@@ -0,0 +1,8 @@
+export default function Menu() {
+  return (
+    <ul className="menu">
+      <li>Home</li>
+      <li>Sobre</li>
+    </ul>
+  );
+}
